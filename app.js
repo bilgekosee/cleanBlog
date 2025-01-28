040a2896e266612d67fe16ff85ec09fb8482cc8d
bilgekosee <bilge.koosee@gmail.com>
2025-01-28 14:02:13 +0300

    express and nodemon added

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const blog = { id: 1, title: 'Blog title', description: 'Blog description' };
+app.get('/', (req, res) => {
+  res.send(blog);
+});
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portuna bağlandı`);
+});
